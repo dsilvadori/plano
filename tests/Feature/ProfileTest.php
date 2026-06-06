84c6a0688d5c951b56d045528f4a0900321c5568
dsilvadori <devmaquina.ds@gmail.com>
2026-06-06 20:34:15 -0300

    Remove exclusao de conta do perfil do aluno

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -21,6 +21,20 @@ class ProfileTest extends TestCase
         $response->assertOk();
     }
 
+    public function test_profile_page_does_not_show_account_deletion_option(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->get('/profile');
+
+        $response
+            ->assertOk()
+            ->assertDontSee('Delete Account')
+            ->assertDontSee('confirm-user-deletion');
+    }
+
     public function test_profile_information_can_be_updated(): void
     {
         $user = User::factory()->create();
@@ -61,7 +75,7 @@ class ProfileTest extends TestCase
         $this->assertNotNull($user->refresh()->email_verified_at);
     }
 
-    public function test_user_can_delete_their_account(): void
+    public function test_user_cannot_delete_their_account_from_profile(): void
     {
         $user = User::factory()->create();
 
@@ -71,28 +85,7 @@ class ProfileTest extends TestCase
                 'password' => 'password',
             ]);
 
-        $response
-            ->assertSessionHasNoErrors()
-            ->assertRedirect('/');
-
-        $this->assertGuest();
-        $this->assertNull($user->fresh());
-    }
-
-    public function test_correct_password_must_be_provided_to_delete_account(): void
-    {
-        $user = User::factory()->create();
-
-        $response = $this
-            ->actingAs($user)
-            ->from('/profile')
-            ->delete('/profile', [
-                'password' => 'wrong-password',
-            ]);
-
-        $response
-            ->assertSessionHasErrorsIn('userDeletion', 'password')
-            ->assertRedirect('/profile');
+        $response->assertStatus(405);
 
         $this->assertNotNull($user->fresh());
     }
